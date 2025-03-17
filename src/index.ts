2b90d6ad4eb56f4a05d0d7bc67467f52b0702c01
Sabyasachi <saby@stackbox.xyz>
2025-03-17 17:29:06 +0530

    :sparkles: event-consumers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import FpEventBus from "./event-bus";
 export * from "./event-bus/interfaces";
 
-const Plugins = {
+export const Plugins = {
   EventBus: FpEventBus,
 };
 
-export default Plugins;
+export * from "./event-bus/event-consumer";

f61493bbff7fbe0b81717c49772661855af3ca8d
Sabyasachi <saby@stackbox.xyz>
2025-03-18 13:56:24 +0530

    :bug: missed library export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import FpEventBus from "./event-bus";
+import FpFileStore from "./file-store";
+
 export * from "./event-bus/interfaces";
+export { FileStore } from "./file-store";
 
 export const Plugins = {
   EventBus: FpEventBus,
+  FileStore: FpFileStore,
 };
 
 export * from "./event-bus/event-consumer";
